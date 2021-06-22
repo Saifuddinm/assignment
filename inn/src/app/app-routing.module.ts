e9c1541b1a4f0905d8fc5d02f2c20872a09801c4
Saifuddinm <84314120+Saifuddinm@users.noreply.github.com>
2021-06-22 22:27:55 +0530

    update
    updates

--- inn/src/app/app-routing.module.ts
+++ inn/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
 import { TableComponent } from './page/table/table.component';
+import { UserComponent } from './page/user/user.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'table', component: TableComponent},
+  {path: 'user', component: UserComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
 
